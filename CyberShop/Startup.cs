893a0e22187b1d64cbe22e757aae438e16aa7784
victor.bernardino@uabc.edu.mx <victor.bernardino@uabc.edu.mx>
2021-10-05 19:28:08 -0700

    Agregar archivos de proyecto.

---
+++ CyberShop/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CyberShop.Startup))]
+namespace CyberShop
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
