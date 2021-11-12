c702620c6d360371cb73d409ca27e592fd4aa8f6
ortega.samuel@uabc.edu.mx <ortega.samuel@uabc.edu.mx>
2021-11-11 19:41:29 -0800

    cambios

--- CyberShop/Models/Model.Designer.cs
+++ CyberShop/Models/Model.Designer.cs
@@ -1,4 +1,4 @@
-﻿// La generación de código T4 está habilitada para el modelo 'C:\Users\kike\Source\Repos\CyberShop2\CyberShop\Models\Model.edmx'. 
+﻿// La generación de código T4 está habilitada para el modelo 'C:\Users\sam_a\Source\Repos\CyberShop\CyberShop\Models\Model.edmx'. 
 // Para habilitar la generación de código heredada, cambie el valor de la propiedad del diseñador 'Estrategia de generación de código'
 // por 'ObjectContext heredado'. Esta propiedad está disponible en la ventana Propiedades cuando se abre
 // el modelo en el diseñador.
